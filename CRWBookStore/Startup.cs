f0d7771526da1aec59de9964b3e6a240d519672a
Mengni Wu <nikowu@vrv.earth>
2022-02-23 21:52:53 -0800

    AddShoppingCart almost work

--- CRWBookStore/Startup.cs
+++ CRWBookStore/Startup.cs
@@ -2,6 +2,7 @@ using CRWBookStore.Data;
 using CRWBookStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -32,7 +33,10 @@ namespace CRWBookStore
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
