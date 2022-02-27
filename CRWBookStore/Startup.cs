f375a8ec2fe876eecda503b2141f09ee5fa1864a
Mengni Wu <nikowu@vrv.earth>
2022-02-27 14:19:55 -0800

    Shopping cart work. remove button work

--- CRWBookStore/Startup.cs
+++ CRWBookStore/Startup.cs
@@ -41,6 +41,7 @@ namespace CRWBookStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseAuthentication();
