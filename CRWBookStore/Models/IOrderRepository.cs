22e35c1284572975b7c54c37c9e16f12813df546
Mengni Wu <nikowu@vrv.earth>
2022-03-01 19:02:46 -0800

    follow all step with book, but can't find bug, so the check out button still not work.

---
+++ CRWBookStore/Models/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using CRWBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CRWBookStore.Models
+{
+    public interface IOrderRepository
+    {
+
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
